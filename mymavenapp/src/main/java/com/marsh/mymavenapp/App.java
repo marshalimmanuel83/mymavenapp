201e4c38dd9a97559d067971abd8dabde98cfe49
Marshal Immanuel <34843530+marshalimmanuel83@users.noreply.github.com>
2020-02-21 12:31:49 +0530

    Added files via upload
    This is a fundamental Maven project to play around and learn how the dependencies are getting downloaded from Maven Central repository.

---
+++ mymavenapp/src/main/java/com/marsh/mymavenapp/App.java
@@ -0,0 +1,13 @@
+package com.marsh.mymavenapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World of Maven" );
+    }
+}
